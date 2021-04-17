de0dc8c07f083501d52c55f22aead19e92cbecd7
Natalie Johansson <mnrjohansson@gmail.com>
2021-04-17 20:15:13 +0300

    tests

---
+++ src/tests/card_test.py
@@ -0,0 +1,32 @@
+import unittest
+from card import Card
+
+class TestCard(unittest.TestCase):
+    def setUp(self):
+        self.card = Card(1,0,0)
+    
+    def test_card_is_not_shown_when_created(self):
+        self.assertFalse(self.card.shown)
+    
+    def test_card_is_shown_when_flipped(self):
+        self.card.flip()
+        self.assertTrue(self.card.shown)
+
+    def test_card_is_not_shown_when_flipped_twice(self):
+        self.card.flip()
+        self.card.flip()
+        self.assertFalse(self.card.shown)
+
+    def test_number_matches_card_when_created(self):
+        self.assertEqual(1,self.card.nr)
+
+    def test_coordinates_match_cards_when_created(self):
+        self.assertEqual(0,self.card.x)
+        self.assertEqual(0,self.card.y)
+        self.assertEqual(80,self.card.x2)
+        self.assertEqual(100,self.card.y2)
+
+    def test_get_pos_return_correct_values(self):
+        self.assertEqual((0,0),self.card.get_pos())
+
+        
--- src/tests/deck_test.py
+++ src/tests/deck_test.py
@@ -5,8 +5,14 @@ class TestDeck(unittest.TestCase):
     def setUp(self):
         self.deck1=Deck(1)
         self.deck2=Deck(2)
+        self.deck3=Deck(3)
 
     def test_deck_is_random_when_created(self):
         easydeck1 = Deck(1).deck
         easydeck2 = Deck(1).deck
         self.assertFalse(self.deck1.deck == easydeck1 == easydeck2)
+
+    def decks_have_correct_number_of_cards(self):
+        self.assertEqual(len(self.deck1.deck),12)
+        self.assertEqual(len(self.deck2.deck),20)
+        self.assertEqual(len(self.deck3.deck),30)
