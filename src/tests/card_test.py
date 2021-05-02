8329954557e8728d72d2df31b13d7f152af74535
Natalie Johansson <mnrjohansson@gmail.com>
2021-05-02 16:05:40 +0300

    fixed tests

--- src/tests/card_test.py
+++ src/tests/card_test.py
@@ -18,13 +18,13 @@ class TestCard(unittest.TestCase):
         self.assertFalse(self.card.shown)
 
     def test_number_matches_card_when_created(self):
-        self.assertEqual(1, self.card.nr)
+        self.assertEqual(1, self.card.number)
 
     def test_coordinates_match_cards_when_created(self):
         self.assertEqual(0, self.card.x)
         self.assertEqual(0, self.card.y)
-        self.assertEqual(80, self.card.x2)
-        self.assertEqual(100, self.card.y2)
+        self.assertEqual(80, self.card.x_2)
+        self.assertEqual(100, self.card.y_2)
 
     def test_get_pos_return_correct_values(self):
         self.assertEqual((0, 0), self.card.get_pos())
